2bdd03623aa015eff91d94afadd6aab74e4ddbc5
Paul Montoussy <paul.montoussy@gmail.com>
2025-05-19 16:56:10 +0200

    début de mon php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="image/menuicon.png" alt="Menu Dropdown" id="header__menuLogo">
+        <img src="image/icondofus.svg" alt="Logo page" id="header__pageLogo">
+        <div id="header__title">Marché de Kérubim</div>
+    </header>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "Welcome in my first PHP project" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kerubim's Shop</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="/image/icondofus.svg">
+</head>
+
+<body>
+    <?php include('header.php'); ?>
+
+</body>
+
+</html>
